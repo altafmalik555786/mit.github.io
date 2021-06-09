df5c6a1e2d4697707185b6ad7a364b8f46de16a4
altafmalik555786 <altafmalik555786@gmail.com>
2021-06-09 15:40:38 +0500

    Publishing my first website
    Wow I'm happy

---
+++ about.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,200&display=swap" rel="stylesheet">
+</head>
+
+<body>
+   
+    <?php include 'menu.php' ?>
+
+    <div class="jumbotron">
+        <h1>MIT Network</h1>
+        <p>We are dealing with Web-Development, Graphic Designing and CMS - Content Management System WordPress Developement .</p>
+    </div>
+
+    <section class="my-5">
+        <div class="py-5">
+            <h1 class="text-center">About Us</h1>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-12">
+                    <img src="images/about.jpg" class="img-fluid aboutimg">  
+                </div>
+                <div class="col-lg-6 col-md-6 col-12">
+                    <h2 class="display-4">MIT Network provides professional IT services</h2>
+                    <p class="py-5">MIT Network has expert team of Information Technology.
+                    Our team has experties in different fields including Web-Development, CMS - Content Management System and Graphic Designing. Our team members are highly qualified and experienced</p>
+                    <a href="about.php" class="btn btn-success">Check More</a> 
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <footer>
+        <p class="p-3 bg-dark text-white text-center">@WebMartGroupofAltafCompnay</p>
+    </footer>
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
